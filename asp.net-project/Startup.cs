70f40647e05db3ad4a422ce2accc8a3997d89ade
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-09-13 10:03:24 +0300

    View, configuration, properties.

---
+++ asp.net-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_project.Startup))]
+namespace asp.net_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

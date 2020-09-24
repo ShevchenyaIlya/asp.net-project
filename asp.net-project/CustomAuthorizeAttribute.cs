2996b167bddb477456b88578ca87bd7b1d99c474
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-09-24 11:50:21 +0300

    Custom authorization class.

---
+++ asp.net-project/CustomAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp.net_project
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string ViewName { get; set; }
+
+        public CustomAuthorizeAttribute()
+        {
+            ViewName = "AuthorizationFailed";
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            IsUserAuthorized(filterContext);
+        }
+        void IsUserAuthorized(AuthorizationContext filterContext)
+        {
+            //User authorized
+            if (filterContext.Result == null)
+                return;
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                ViewDataDictionary dictionary = new ViewDataDictionary();
+                dictionary.Add("Message", "You don't have sufficient privileges for this operation!");
+                var result = new ViewResult() { ViewName = this.ViewName, ViewData = dictionary };
+                filterContext.Result = result;
+            }
+        }
+    }
+}
